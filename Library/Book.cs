d05cd00ed75ddcc49f0e693d198f934f949e18e8
Callum McConnell <McConnell-C28@ulster.ac.uk>
2024-10-08 12:03:04 +0100

    got to 24T.

--- Library/Book.cs
+++ Library/Book.cs
@@ -4,19 +4,42 @@
     {
 
         // Properties
-        string Title;
-        string Author;
-        string ISBN;
-        int NoOfPages;
+        private string Title;
+        private string Author;
+        private string ISBN;
+        private int NoOfPages;
+
+        // Title property to allow access
+        // to the title private variable
+        public string Title
+        {
+            get { return Title; } // Get method
+            set { Title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; } // Get method
+            set { author = value; } // set method
+        }
+        public string ISBN
+        {
+            get { return isbn; } // Get method
+            set { isbn = value; } // set method
+        }
+        public int NoOfPages
+        {
+            get { return noofpages; } // Get method
+            set { noofpages = value; } // set method
+        }
 
         // Constructor for book object
         // add new instance of the book class
         public Book(string bookTitle, string bookAuthor, string bookISBN, int bookNoOfPages)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
-            NoOfPages = bookNoOfPages;
+            this.title = bookTitle;
+            this.author = bookAuthor;
+            this.isbn = bookISBN;
+            this.noofpages = bookNoOfPages;
         }
 
         public void DisplayInfo()
