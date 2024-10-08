42c98d60f942d73dcf899e41110fc2591bc89bfb
Callum McConnell <McConnell-C28@ulster.ac.uk>
2024-10-08 11:54:05 +0100

    Updated code with access modifiers.

---
+++ Library/Book.cs
@@ -0,0 +1,34 @@
+﻿namespace Library
+{
+    class Book
+    {
+
+        // Properties
+        string Title;
+        string Author;
+        string ISBN;
+        int NoOfPages;
+
+        // Constructor for book object
+        // add new instance of the book class
+        public Book(string bookTitle, string bookAuthor, string bookISBN, int bookNoOfPages)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+            NoOfPages = bookNoOfPages;
+        }
+
+        public void DisplayInfo()
+        {
+            // Outbook the book information to the console
+            Console.WriteLine("Book information");
+            Console.WriteLine("----------------");
+            Console.WriteLine($"Book Title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine($"Number of pages: {NoOfPages}");
+            Console.WriteLine();
+        }
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,34 +1,6 @@
-﻿class Book
+﻿using Library;
+class Program
 {
-
-    // Properties
-    string Title;
-    string Author;
-    string ISBN;
-    int NoOfPages;
-
-    // Constructor for book object
-    // add new instance of the book class
-    public Book(string bookTitle, string bookAuthor, string bookISBN, int bookNoOfPages)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-        NoOfPages = bookNoOfPages;
-    }
-
-    void DisplayInfo()
-    {
-        // Outbook the book information to the console
-        Console.WriteLine("Book information");
-        Console.WriteLine("----------------");
-        Console.WriteLine($"Book Title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine($"Number of pages: {NoOfPages}");
-        Console.WriteLine();
-    }
-
     static void Main(string[] args)
     {
         // Create a new instance of the book class
